39bcfa119b6113c993c7467eb813721aa0a8de48
Vinicius Grillo <vini_grillo@hotmail.com>
2024-05-11 18:08:33 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/vinigrillo/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/vinigrillo/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
+import com.vinigrillo.cursomc.services.exceptions.DataIntegrityViolation;
 import com.vinigrillo.cursomc.services.exceptions.ObjectNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,4 +19,10 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
+	@ExceptionHandler(DataIntegrityViolation.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityViolation e, HttpServletRequest request){	
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(),e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
